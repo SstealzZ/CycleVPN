e2524ad7f5cc3f78882443fc46c779f2ab73b04f
sstealzz <antoine.veber@epitech.eu>
2024-04-14 17:43:11 +0200

    Modify main.py - Add logging and color

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ import tempfile
 import subprocess
 import signal
 import time
+import logging
+from colorama import init, Fore, Style
+
+# Initialize colorama to support ANSI escape character sequences for colored output
+init(autoreset=True)
+
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.INFO, format='[%(levelname)s] %(message)s')
 
 def get_list_of_ovpn_files(path):
     ovpn_files = []
     for file in os.listdir(path):
         if file.endswith(".ovpn"):
             ovpn_files.append(file)
-            print("Found ovpn file: " + file)
+            logging.info(f"Found ovpn file: {file}")  # Log the found ovpn file
     return ovpn_files
 
-def connect_to_ovpn(profile_path, username, password, time):
+def connect_to_ovpn(profile_path, username, password, timeout):
+    logging.info(f"Connecting to {profile_path}...")  # Log the connection attempt
+    print(Fore.GREEN + f"[INFO] Connecting to {profile_path}..." + Style.RESET_ALL)  # Print info message in console
     # Create a temporary file to store username and password
     temp_file = tempfile.NamedTemporaryFile(delete=False)
     temp_file.write(f"{username}\n{password}".encode())
@@ -25,12 +35,16 @@ def connect_to_ovpn(profile_path, username, password, time):
 
     # Wait for the process to finish (or be interrupted)
     try:
-        process.wait(timeout= time)  # Adjust timeout as needed
+        process.wait(timeout=timeout)  # Adjust timeout as needed
+        logging.info("Connection successful.")  # Log successful connection
+        print(Fore.GREEN + "[INFO] Connection successful." + Style.RESET_ALL)  # Print success message in console
     except subprocess.TimeoutExpired:
         # If timeout expires, kill the process
         process.terminate()
-        
-        ## Procces to kill the openvpn process (if needed)
+        logging.error("Connection timed out.")  # Log connection timeout
+        print(Fore.RED + "[ERROR] Connection timed out." + Style.RESET_ALL)  # Print error message in console
+
+        ## Process to kill the OpenVPN process (if needed)
         try:
             process.wait(timeout=10)  # Wait for the process to terminate gracefully
         except subprocess.TimeoutExpired:
